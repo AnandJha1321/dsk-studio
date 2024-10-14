4cbe484d2dbf56a739342ea755093f5891536a5a
AnandJha1321 <anandprojects1321@gmail.com>
2024-10-14 20:07:44 +0530

    Courses

--- src/components/courses/Coursebox1.jsx
+++ src/components/courses/Coursebox1.jsx
@@ -10,24 +10,12 @@ import course6 from '@/assets/course6.jpg';
 import course5 from '@/assets/course5.jpg';
 import CoursesButton from '../CoursesButton';
 import BookingButton from './BookingButton';
-import { PhoneIcon } from '@heroicons/react/solid';
+import { CheckCircleIcon, DownloadIcon, PhoneIcon } from '@heroicons/react/solid';
 
 const CourseList = () => {
 
 const courses = [
-  {
-    id: 'basic-to-international',
-    title: 'Basic to International',
-    selectName: 'Basic to International',
-    price: '6000',
-    description: 'Everything you need to makeup a bridal, from Haldi to Reception, this course focuses on making you a Bridal Artist even if you are a complete beginner.',
-    image: course2,
-    contents: [
-      'Haldi',
-      'Wedding',
-      'Reception / Engagement',
-    ],
-  },
+
   {
     id: 'bridal-course',
     title: 'Bridal Course',
@@ -38,7 +26,25 @@ const courses = [
     contents: [
       'Haldi',
       'Wedding',
-      'Reception / Engagement',
+      'Reception',
+      'Engagement',
+    ],
+  },
+  {
+    id: 'basic-to-international',
+    title: 'Basic to International',
+    selectName: 'Basic to International',
+    price: '6000',
+    description: 'Everything you need to makeup a bridal, from Haldi to Reception, this course focuses on making you a Bridal Artist even if you are a complete beginner.',
+    image: course2,
+    contents: [
+      'Bridal Course',
+      'Nude Makeup',
+      'Fantasy Makeup',
+      'Film & Video Makeup',
+      'HD Makeup',
+      'Corrective Makeup',
+      'And More...'
     ],
   },
   {
@@ -49,9 +55,11 @@ const courses = [
     description: 'Everything you need to makeup a bridal, from Haldi to Reception, this course focuses on making you a Bridal Artist even if you are a complete beginner.',
     image: course3,
     contents: [
-      'Haldi',
-      'Wedding',
-      'Reception / Engagement',
+      'Open hairstyle  (Curls, Tongs, Straighteneing)',
+      'Front Variation',
+      'Braids',
+      'Hair Buns (Indian Bun, Messy Bun)',
+      'International Hairstyle'
     ],
   },
   {
@@ -62,9 +70,16 @@ const courses = [
     description: 'Everything you need to makeup a bridal, from Haldi to Reception, this course focuses on making you a Bridal Artist even if you are a complete beginner.',
     image: course4,
     contents: [
-      'Haldi',
-      'Wedding',
-      'Reception / Engagement',
+      'Bridal Course',
+      'Hairstyle Course',
+      'Hair Colouring',
+      'Highlights',
+      'Waxing',
+      'Facial & Cleanups',
+      'Eyebrow',
+      'Bleach',
+      'Hair Spa',
+      'Hair treatments',
     ],
   },
   {
@@ -75,22 +90,32 @@ const courses = [
     description: 'Everything you need to makeup a bridal, from Haldi to Reception, this course focuses on making you a Bridal Artist even if you are a complete beginner.',
     image: course5,
     contents: [
-      'Haldi',
-      'Wedding',
-      'Reception / Engagement',
+      'Nude Makeup',
+      'Bridal Makeup',
+      'Party Makeup',
+      'Airbrush Makeup',
+      'HD Makeup',
+      'Runway Catwalk'
     ],
   },
   {
     id: 'hair-treatment',
-    title: 'Hair Treatment ',
+    title: 'Hair Treatment Course',
     selectName: 'Hair Treatment',
     price: '6000',
     description: 'Everything you need to makeup a bridal, from Haldi to Reception, this course focuses on making you a Bridal Artist even if you are a complete beginner.',
     image: course6,
     contents: [
-      'Haldi',
-      'Wedding',
-      'Reception / Engagement',
+      'Hair Cuts',
+      'Global',
+      'Balayage',
+      'Highlights',
+      'Botox',
+      'Nanoplastia',
+      'Keratin', 
+      'Smoothening',
+      'Kerasmootheing',
+      'Hair Spa'
     ],
   },
 ];
@@ -115,7 +140,7 @@ const courses = [
       {/* Dropdown for selecting courses */}
       <div className="flex justify-center mt-4">
         <select
-          className="bg-gray-200 text-black text-sm p-2 font-montserrat rounded-md"
+          className="bg-gray-200 md:hidden text-black text-sm p-2 font-montserrat rounded-md"
           value={selectedCourse}
           onChange={handleCourseChange}
         >
@@ -127,10 +152,10 @@ const courses = [
         </select>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-8 mt-10">
+      <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 gap-8 md:gap-3 mt-10">
         {courses.map((course, index) => (
           <div id={course.id} key={index} className='flex flex-col items-center'>
-            <CourseCard course={course} />
+            <CourseCard course={course} id={index} />
             {index === 2 && (
               <div className='mt-8 md:hidden'>
                 <BookingButton />
@@ -143,9 +168,9 @@ const courses = [
   );
 };
 
-const CourseCard = ({ course }) => {
+const CourseCard = ({ course, id }) => {
   return (
-    <section className="relative w-[100%] h-80">
+    <section className={`relative w-[100%] h-[350px] `}>
       {/* Background Image */}
       <Image
         src={course.image}
@@ -156,25 +181,37 @@ const CourseCard = ({ course }) => {
       />
       
       {/* Overlay with text */}
-      <div className="absolute inset-0 bg-black bg-opacity-75 flex flex-col py-6 px-6">
+      <div className="absolute inset-0 bg-black bg-opacity-75 flex flex-col py-4 px-6">
         <div className="my-3">
           <h1 className="text-[#cfb336] text-[24px] leading-tight mb-1 font-medium font-montserrat w-[334px]">
             {course.title}
           </h1>
-          <p className="text-gray-300 text-xs font-montserrat w-[320px] mb-4">
+          <p className="text-gray-300 text-xs  font-montserrat w-[320px] mb-4 sm:text-sm  sm:w-[400px]  ">
             {course.description}
           </p>
-          <ul className="list-disc list-inside mb-4">
+          <ul className={`${id === 2 
+          ? "w-full pb-3 sm:grid sm:grid-cols-2 lg:block"
+            : "w-[90%] sm:w-[70%] md:w-[70%] text-nowrap grid grid-cols-2 mb-4  "}`}>
             {course.contents.map((item, index) => (
-              <li key={index} className="text-white">{item}</li>
+              <li key={index} className="text-white list-none font-lato text-left " >
+                <p className= 'flex items-center gap-3 text-[14px] md:text-base'>    
+                  <span>
+                  <CheckCircleIcon className={`${item === 'Bridal Course' || item === 'Hairstyle Course' ? 'w-5' : 'w-4'} text-yellow-400`} />
+                    </span>             
+                  {item}
+                </p>
+                </li>
             ))}
           </ul>
-          <div className='flex'>
-            <button className="bg-white flex text-black px-2 items-center justify-center text-[14px] py-2 font-montserrat hover:bg-gray-200 transition duration-200">
+          <div className='flex '>
+            <button className="bg-white flex text-black px-2 items-center justify-center text-[14px] py-2 
+            font-montserrat hover:bg-gray-200 transition duration-200  text-nowrap">
               <span className="mr-2"><PhoneIcon className='w-4'/></span>
               Call Now For 10% off
             </button>
-            <button className="bg-black flex text-white ml-3 px-3 items-center hover:text-black justify-center text-[14px] py-2 font-montserrat hover:bg-gray-200 transition duration-200">Learn More</button>
+            <button className="bg-black flex text-white ml-3 px-3 items-center hover:text-black justify-center text-[14px]
+             py-2 font-montserrat hover:bg-gray-200 transition duration-200  text-nowrap"
+             > <span><DownloadIcon className='w-4 mr-2'/></span>Get Syllabus</button>
           </div>
         </div>
       </div>
