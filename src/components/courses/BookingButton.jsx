62000246cd5e60d62055f005d6b51267ccf284d0
AnandJha1321 <anandprojects1321@gmail.com>
2024-11-06 22:14:27 +0530

    Done

--- src/components/courses/BookingButton.jsx
+++ src/components/courses/BookingButton.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const BookingButton = () => {
   return (
-   <a href="tel:+9867035747">
+   <a href="tel:+919867035747">
     <div className=' py-3  bg-[#cfb336] hover:bg-[#A88C13] font-montserrat text-white transition duration-150 '>
-        <p className='text-center md:text-[20px] px-6'>Contact To Book Your Spot</p>
+        <p className='text-center md:text-[20px] px-6'>Book Your Spot Now</p>
     </div>
    </a>
   )
